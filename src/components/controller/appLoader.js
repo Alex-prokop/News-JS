51856cc508791c21d45379d0ba1352dc5ee886be
Alexandr Prokopovich <prokop7@mail.ru>
2023-06-07 17:05:51 +0300

    feat: add api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'ab245c2f8a53458781466cedebb4eb87',
         });
     }
 }
