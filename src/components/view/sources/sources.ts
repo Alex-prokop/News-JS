c77e3ef39ad5a2abeceebe4e09e4e5ad6a91cd30
Alexandr Prokopovich <prokop7@mail.ru>
2023-06-08 12:53:06 +0300

    feat: sources.ts

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -1,20 +1,28 @@
 import './sources.css';
 
+interface SourceData {
+    name: string;
+    id: string;
+}
 class Sources {
-    draw(data) {
+    draw(data: SourceData[]): void {
         const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+        if (!sourceItemTemp) {
+            throw new Error('sourceItemTemp is null');
+        }
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+        data.forEach((item: SourceData) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources').append(fragment);
+        (document.querySelector('.sources') as HTMLElement).append(fragment);
     }
 }
 
